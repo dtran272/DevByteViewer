e20fa888a265c32786a2e8cf18c24d1c812ced1d
DK Tran <d.chan272@gmail.com>
2022-08-30 08:55:35 -0400

    - Add DatabaseVideo entity to DatabaseEntities.kt - Add extension function to convert Network results of videos to DatabaseVideo object - Add extension function DatabaseVideo object to domain Video model - Add VideDao interface to Room.kt - Add VideoDatabase tp Room.kt

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,3 +17,33 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.Video
+
+@Entity
+data class DatabaseVideo constructor(
+    @PrimaryKey
+    val url: String,
+    val updated: String,
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)
+
+/**
+ * Converts DatabaseVideo object to domain model Video.
+ *
+ * @return List of Video domain model
+ */
+fun List<DatabaseVideo>.asDomainModel(): List<Video> {
+    return map {
+        Video(
+            url = it.url,
+            title = it.title,
+            description = it.description,
+            updated = it.updated,
+            thumbnail = it.thumbnail
+        )
+    }
+}
--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -16,3 +16,37 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface VideoDao {
+    @Query("select * from databasevideo")
+    suspend fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideoDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideoDatabase
+
+fun getDatabase(context: Context): VideoDatabase {
+    synchronized(VideoDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideoDatabase::class.java,
+                "videos_database"
+            ).build()
+        }
+    }
+
+    return INSTANCE
+}
